3bd112040e7bea0091133c78f74c6f2ae7cb54b1
Kenjaeff <jamol.0315.man4ik@gmail.com>
2024-03-18 00:13:48 +0500

    feat: solve 1-4 tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,10 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+// 1
+function shallowCopy(obj) {
+  const emptyObj = {};
+  return Object.assign(emptyObj, obj);
 }
 
 /**
@@ -32,8 +34,15 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+// 2
+function mergeObjects(objects) {
+  return objects.reduce((merged, obj) => {
+    const newMerged = { ...merged };
+    Object.entries(obj).forEach(([key, value]) => {
+      newMerged[key] = (newMerged[key] || 0) + value;
+    });
+    return newMerged;
+  }, {});
 }
 
 /**
@@ -49,8 +58,16 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+// 3
+function removeProperties(obj, keysToRemove) {
+  const keys = Array.isArray(keysToRemove) ? keysToRemove : [keysToRemove];
+  const result = { ...obj };
+
+  keys.forEach((key) => {
+    delete result[key];
+  });
+
+  return result;
 }
 
 /**
@@ -65,8 +82,16 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+// 4
+function compareObjects(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  return keys1.every((key) => obj1[key] === obj2[key]);
 }
 
 /**
