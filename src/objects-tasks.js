5b7830e04a8ca19d8891e1db6271962dc3f17e4b
Kenjaeff <jamol.0315.man4ik@gmail.com>
2024-03-18 06:19:10 +0500

    feat: solve 5-8 tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -105,8 +105,9 @@ function compareObjects(obj1, obj2) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+// 5
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
 }
 
 /**
@@ -125,8 +126,9 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+// 6
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -139,8 +141,15 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+// 7
+function makeWord(lettersObject) {
+  return Object.entries(lettersObject)
+    .flatMap(([letter, positions]) =>
+      positions.map((position) => [position, letter])
+    )
+    .sort(([position1], [position2]) => position1 - position2)
+    .map(([, letter]) => letter)
+    .join('');
 }
 
 /**
@@ -157,8 +166,20 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+// 8
+function sellTickets(queue) {
+  let money = 0;
+
+  return queue.every((payment) => {
+    if (payment <= 25) {
+      money += payment;
+      return true;
+    }
+    const change = payment - 25;
+    const canGiveChange = money >= change;
+    money += 25;
+    return canGiveChange;
+  });
 }
 
 /**
